c348f2eaf199eec745bc4b9cdd4d8bba113b8fdc
r0cknr011a <200347677+r0cknr011a-322@users.noreply.github.com>
2026-01-08 15:06:52 +0300

    cmd queue API is OK, i guess

--- toolkit/src/cmd/rw.rs
+++ toolkit/src/cmd/rw.rs
@@ -1,20 +1,3 @@
-use crate::cmd::{ Queue };
-use toolkit_unsafe::{ IPCByteBuf };
-
-pub trait BufRefQueue: Queue {
-    type Request = IPCByteBuf;
-    type Response = Response;
-    type Error = Error;
-
-    fn push(&mut self, req: Self::Request) -> Poll<Result<(), Self::Error>> {
-        self.push(req)
-    }
-
-    fn pop(&mut self) -> Poll<Result<Self::Response, Self::Error>> {
-        self.pop()
-    }
-}
-
 pub enum Response {
     Ok,
 }
--- toolkit/src/ip.rs
+++ toolkit/src/ip.rs
@@ -1,54 +1,60 @@
 use crate::collection::deque::{ Deque };
 use crate::cmd::{ Queue, Poll };
-use crate::cmd::rw::{ Request, Response, Error };
+use crate::cmd::rw::{ Response, Error };
 use toolkit_unsafe::{ IPCByteBuf };
 
-pub struct IPRefQueue
-<OQ, const OUTLEN: usize, IQ, const INLEN: usize>
-{
-    outq: OQ,
-    outreq: Deque<IPCByteBuf, REQNR>,
-    outrsp: Deque<Response, RSPNR>,
-    inq: IQ,
-    rspbuf: Deque<IPCByteBuf, NR>,
+struct IPCBufQueue<Q, const REQNR: usize, const RSPNR: usize> {
+    queue: Q,
+    reqbuf: Deque<IPCByteBuf, REQNR>,
+    rspbuf: Deque<Response, RSPNR>,
 }
 
-impl
-<ORQ, IRQ, OBQ, IBQ,
-const REQNR: usize, const RSPNR: usize,
-const WRLEN: usize, const RDLEN: usize>
-IPQueue<ORQ, IRQ, OBQ, IBQ, REQNR, RSPNR, WRLEN, RDLEN> {
-    pub fn new(rwqueue: Q) -> Self {
+impl<Q, const REQNR: usize, const RSPNR: usize>
+IPCBufQueue<Q, REQNR, RSPNR> {
+    fn new(queue: Q) -> Self {
         Self {
-            rwqueue: rwqueue,
-            reqbuf: Deque::new(|_| RWReq::Read(IPCByteBuf::new(0, 0))),
-            rspbuf: Deque::new(|_| RWRsp::Ok),
-            wrbuf: Deque::default(), rdbuf: Deque::default(),
+            queue: queue,
+            reqbuf: Deque::new(|_| IPCByteBuf::new(0, 0)),
+            rspbuf: Deque::new(|_| Response::Ok),
         }
     }
 }
 
+/*
+ * send queue
+ */
+pub struct SendIPCBufQueue<Q, const REQNR: usize, const RSPNR: usize> {
+    queue: IPCBufQueue<Q, REQNR, RSPNR>,
+}
 
-impl<Q,
-const REQNR: usize, const RSPNR: usize,
-const WRLEN: usize, const RDLEN: usize>
-BufRefQueue for IPQueue<Q, REQNR, RSPNR, WRLEN, RDLEN> {
-where Q: Queue<Shit> {
-    type Response = Deque<RWRsp, CMDNR>;
-    type Error = RWErr;
-
-    fn push(&mut self, reqbuf: Req) -> Poll<Result<(), Self::Error>> {
-        for req in reqbuf {
-            if let RWReq::Read(buf) = req {
-                if buf.len() < 128 {
-                    return Poll::Ready(Err(RWErr::Fatal));
-                }
-            }
+impl<Q, const REQNR: usize, const RSPNR: usize>
+SendIPCBufQueue<Q, REQNR, RSPNR> {
+    pub fn new(queue: Q) -> Self {
+        Self {
+            queue: IPCBufQueue::new(queue),
         }
-        Poll::Ready(Ok(()))
+    }
+}
+
+impl<Q, const REQNR: usize, const RSPNR: usize>
+Queue for SendIPCBufQueue<Q, REQNR, RSPNR>
+where Q: Queue<Request=IPCByteBuf, Response=Response, Error=Error> {
+    type Request = IPCByteBuf;
+    type Response = Response;
+    type Error = Error;
+
+    fn push(&mut self, _req: IPCByteBuf) -> Poll<Result<(), Error>> {
+        Poll::Ready(Err(Error::Fatal))
     }
 
-    fn pop(&mut self) -> Poll<Result<Self::Response, Self::Error>> {
-        Poll::Ready(Ok(self.rspbuf))
+    fn pop(&mut self) -> Poll<Result<Response, Error>> {
+        Poll::Ready(Err(Error::Fatal))
     }
+}
+
+/*
+ * recv queue
+ */
+pub struct RecvIPCBufQueue<Q, const REQNR: usize, const RSPNR: usize> {
+    queue: IPCBufQueue<Q, REQNR, RSPNR>,
 }
