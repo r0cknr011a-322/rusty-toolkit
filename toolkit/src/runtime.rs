10b1eca0e590a830f0a1334bfc5f2d06923a58c0
r0cknr011a <200347677+r0cknr011a-322@users.noreply.github.com>
2026-01-10 20:25:55 +0300

    ordinary patch

--- toolkit/src/runtime.rs
+++ toolkit/src/runtime.rs
@@ -1,10 +1,10 @@
 use core::fmt::{ self, Write };
-// use core::cell::{ Cell };
+use core::cell::{ Cell };
 use core::time::{ Duration };
 use crate::collection::deque::{ Deque };
 use crate::collection::string::{ String };
-// use crate::cmd::rw::{ Queue };
-// use toolkit_unsafe::{ RawBuf };
+use crate::cmd::{ Queue };
+use toolkit_unsafe::{ IPCByteBuf };
 
 pub trait Time {
     fn time(&mut self) -> Duration;
@@ -26,7 +26,8 @@ impl<const N: usize, const D: usize> LogChan<N, D> {
     }
 }
 
-impl<const N: usize, const D: usize> Write for LogChan<N, D> {
+impl<const N: usize, const D: usize>
+Write for LogChan<N, D> {
     fn write_str(&mut self, s: &str) -> Result<(), fmt::Error> {
         if self.data.is_full() {
             for _ in 0..s.len() {
@@ -40,43 +41,77 @@ impl<const N: usize, const D: usize> Write for LogChan<N, D> {
     }
 }
 
-/*
-pub struct RuntimeMain <T, Q, const CHANSZ: usize, const CHANNR: usize, const BUFLEN: usize> {
-    logchanbuf: Deque<LogChan<CHANSZ>, CHANNR>,
-    timer: T,
-    rwqueue: Q,
-    rwbufqueue: Deque<RawBuf, BUFLEN>,
+struct LogChanBuf<const CHN: usize, const CHL: usize, const CHNR: usize> {
+    deque: Deque<LogChan<CHN, CHL>, CHNR>,
 }
 
-impl<T, RW,
-const CHANLEN: usize, const CHANNR: usize, const BUFLEN: usize>
-RuntimeInner<T, RW, CHANLEN, CHANNR, BUFLEN>
-{
-    pub fn new<'a, ChanCtr: FnMut(usize) -> &'a str>(
-        timer: T,
-        rwcmd: RW, rwdata: Deque<RawBuf, BUFLEN>,
-        chanmap: ChanCtr
-    ) -> Self {
-        let logchanbuf = Deque::new(|idx| {
-            LogChan::new(chanmap(idx));
-        });
+impl<const CHN: usize, const CHL: usize, const CHNR: usize>
+IntoIterator for LogChanBuf<CHN, CHL, CHNR> {
+    type Item = LogChan<CHN, CHL>;
+    type IntoIter = DequeIter<LogChan<>CHNR>;
+}
+
+impl<const CHN: usize, const CHL: usize, const CHNR: usize>
+LogChanBuf<CHN, CHL, CHNR> {
+    fn new<C: FnMut(usize) -> &'static str>(mut ctr: C) -> Self {
         Self {
-            timer: timer,
-            rwqueue: rwcmd, rwbufqueue: rwdata,
-            logchanbuf: logchanbuf,
+            deque: Deque::new(|idx| LogChan::new(ctr(idx))),
         }
     }
 }
 
-impl<T, Q,
-const CHANLEN: usize, const CHANNR: usize, const BUFLEN: usize>
-RuntimeInner<T, Q, CHANLEN, CHANNR, BUFLEN>
-where T: Time {
-    fn time(&mut self) -> Duration {
-        self.timer.time()
+/*
+ * runtime channel
+ */
+#[derive(Clone, Copy)]
+pub struct RuntimeChan
+<'a, T, Q, const BUFNR: usize,
+const CHN: usize, const CHL: usize, const CHNR: usize> {
+    name: String<CHN>,
+    rt: &'a RuntimeMain<T, Q, BUFNR, CHN, CHL, CHNR>,
+}
+
+impl<'a, T, Q, const BUFNR: usize,
+const CHN: usize, const CHL: usize, const CHNR: usize>
+RuntimeChan<'a, T, Q, BUFNR, CHN, CHL, CHNR> {
+}
+
+/*
+ * runtime
+ */
+pub struct RuntimeMain
+<T, Q, const BUFNR: usize, const CHN: usize, const CHL: usize, const CHNR: usize> {
+    timer: T,
+    queue: Q,
+    bufbuf: Deque<IPCByteBuf, BUFNR>,
+    logchanbuf: LogChanBuf<CHN, CHL, CHNR>,
+}
+
+impl<T, Q, const BUFNR: usize,
+const CHN: usize, const CHL: usize, const CHNR: usize>
+RuntimeMain<T, Q, BUFNR, CHN, CHL, CHNR> {
+    pub fn new<C: FnMut(usize) -> &'static str,
+               B: FnMut(usize) -> IPCByteBuf>
+        (timer: T, queue: Q, mut chanctr: C, mut bufctr: B) -> Self {
+        Self {
+            timer: timer, queue: queue,
+            bufbuf: Deque::new(|idx| bufctr(idx)),
+            logchanbuf: LogChanBuf::new(|idx| chanctr(idx)),
+        }
+    }
+
+    pub fn chan(&self, name: &'static str) ->
+        Option<RuntimeChan<T, Q, BUFNR, CHN, CHL, CHNR>> {
+        for chan in self.logchanbuf {
+            if chan.name == name {
+                return None; 
+            }
+        }
+        None
     }
 }
 
+/*
 impl<T, Q: Queue,
 const CHANLEN: usize, const CHANNR: usize, const BUFLEN: usize>
 Write for RuntimeInner<T, Q, CHANLEN, CHANNR, BUFLEN> {
