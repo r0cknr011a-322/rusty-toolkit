be7d5f013f77a632e5952002087bba574af94d06
r0cknr011a <200347677+r0cknr011a-322@users.noreply.github.com>
2026-01-23 02:20:14 +0300

    another runtime API update

--- toolkit/src/runtime.rs
+++ toolkit/src/runtime.rs
@@ -1,132 +1,136 @@
 use core::fmt::{ self, Write };
-use core::cell::{ Cell, RefCell };
+use core::cell::{ RefCell };
 use core::borrow::{ Borrow, BorrowMut };
 use core::time::{ Duration };
 use crate::collection::deque::{ Deque, DequeRefIter, DequeMutRefIter };
 use crate::cmd::{ Queue };
-use toolkit_unsafe::{ IPCByteBuf };
+use crate::bytebuf::{ MemByteBuf, ByteBuf, VolatileByteBuf, AtomicByteBuf };
 
 pub trait Time {
     fn time(&mut self) -> Duration;
 }
 
-pub trait Runtime: Time + fmt::Write {
-    fn logbuf(&mut self, idx: usize);
-    fn ipcbuf(&mut self, idx: usize);
-
-    fn wr8(&mut self, off: usize, value: u8);
-    fn rd8(&self, off: usize) -> u8;
+pub trait Runtime: Time {
+    fn log<'a>(&'a self, idx: usize) -> Option<impl Write>;
+    fn ipc(&self, idx: usize) -> Option<impl ByteBuf + VolatileByteBuf + AtomicByteBuf>;
+    fn dev(&self, idx: usize) -> Option<impl VolatileByteBuf>;
 }
 
 /*
- * runtime reference
+ * runtime
  */
-#[derive(Clone, Copy)]
-struct RuntimeRef<'a, T, Q, const BUFNR: usize, const CHL: usize, const CHNR: usize> {
-    logbuf: usize,
-    ipcbuf: usize,
-    rt: &'a RuntimeMain<'a, T, Q, BUFNR, CHL, CHNR>,
+pub struct RuntimeMain<'a, T, Q,
+const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize> {
+    timer: RefCell<T>,
+    queue: RefCell<Q>,
+    logbufbuf: RefCell<LogBufBuf<CHL, CHNR>>,
+    ipcbufbuf: RefCell<Deque<MemByteBuf<'a>, IPCBUFNR>>,
+    devmembuf: RefCell<Deque<MemByteBuf<'a>, DEVMEMNR>>,
 }
 
-impl<'a, T, Q, const BUFNR: usize, const CHL: usize, const CHNR: usize>
-Time for RuntimeRef<'a, T, Q, BUFNR, CHL, CHNR>
-where T: Time {
-    fn time(&mut self) -> Duration {
-        let Some(mut timer) = self.rt.timer.take() else {
-            return Duration::default();
-        };
-        let time = timer.time();
-        self.rt.timer.set(Some(timer));
-        time
+impl<'a, T, Q, const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize>
+RuntimeMain<'a, T, Q, IPCBUFNR, DEVMEMNR, CHL, CHNR> {
+    pub fn new<
+        I: FnMut(usize) -> MemByteBuf<'a>,
+        D: FnMut(usize) -> MemByteBuf<'a>,
+    >(timer: T, queue: Q, mut ipcbufctr: I, mut devmemctr: D) -> Self {
+        Self {
+            timer: RefCell::new(timer), queue: RefCell::new(queue),
+            logbufbuf: RefCell::new(LogBufBuf::default()),
+            ipcbufbuf: RefCell::new(Deque::new(|idx| ipcbufctr(idx))),
+            devmembuf: RefCell::new(Deque::new(|idx| devmemctr(idx))),
+        }
     }
 }
 
-impl<'a, T, Q, const BUFNR: usize, const CHL: usize, const CHNR: usize>
-fmt::Write for RuntimeRef<'a, T, Q, BUFNR, CHL, CHNR> {
+impl<'a, T, Q, const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize>
+Time for &RuntimeMain<'a, T, Q, IPCBUFNR, DEVMEMNR, CHL, CHNR>
+where T: Time {
+    fn time(&mut self) -> Duration {
+        let mut timer = self.timer.borrow_mut();
+        timer.time()
+    }
+}
+
+impl<'a, T, Q, const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize>
+Runtime for &RuntimeMain<'a, T, Q, IPCBUFNR, DEVMEMNR, CHL, CHNR>
+where T: Time {
+    fn log<'b>(&self, idx: usize) -> Option<impl Write> {
+        if let Some(_) = self.logbufbuf.borrow().iter().nth(idx) {
+            return Some(LogBufRef { idx: idx, rt: self });
+        }
+        None
+    }
+
+    fn ipc(&self, idx: usize) -> Option<impl ByteBuf + VolatileByteBuf + AtomicByteBuf> {
+        if let Some(_) = self.ipcbufbuf.borrow().iter().nth(idx) {
+            return Some(IPCBufRef { idx: idx, rt: self });
+        }
+        None
+    }
+}
+
+#[derive(Clone, Copy)]
+struct LogBufRef<'a, T, Q,
+const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize> {
+    idx: usize,
+    rt: &'a RuntimeMain<'a, T, Q, IPCBUFNR, DEVMEMNR, CHL, CHNR>,
+}
+
+impl<'a, T, Q, const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize>
+Write for LogBufRef<'a, T, Q, IPCBUFNR, DEVMEMNR, CHL, CHNR> {
     fn write_str(&mut self, s: &str) -> Result<(), fmt::Error> {
-        if let Some(buf) = self.rt.logbufbuf.borrow_mut().iter_mut().nth(self.logbuf) {
+        if let Some(buf) = self.rt.logbufbuf.borrow_mut().iter_mut().nth(self.idx) {
             buf.write_str(s);
         }
         Ok(())
     }
 }
 
-impl<'a, T, Q, const BUFNR: usize, const CHL: usize, const CHNR: usize>
-Runtime for RuntimeRef<'a, T, Q, BUFNR, CHL, CHNR>
-where T: Time {
-    fn logbuf(&mut self, idx: usize) {
-        // let logbuf = self.rt.logbufbuf.take();
-        // if idx < logbuf.iter().len() {
-        //     self.logbuf = idx;
-        // }
-        // self.rt.logbufbuf.set(logbuf);
-        let buf = self.rt.logbufbuf.borrow();
-        if idx < buf.iter().len() {
-            self.logbuf = idx;
-        }
-    }
+#[derive(Clone, Copy)]
+struct IPCBufRef<'a, T, Q,
+const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize> {
+    idx: usize,
+    rt: &'a RuntimeMain<'a, T, Q, IPCBUFNR, DEVMEMNR, CHL, CHNR>,
+}
 
-    fn ipcbuf(&mut self, idx: usize) {
-        let ipcbufbuf = self.rt.ipcbufbuf.take();
-        if let Some(buf) = &ipcbufbuf {
-            if idx < buf.iter().len() {
-                self.ipcbuf = idx;
-            }
-        }
-        self.rt.ipcbufbuf.set(ipcbufbuf);
-    }
-
-    fn rd8(&self, off: usize) -> u8 {
-        let Some(inner) = self.rt.ipcbufbuf.take() else {
-            return 0;
-        };
-        let Some(ipcbuf) = inner.iter().nth(self.ipcbuf) else {
-            return 0;
-        };
-        ipcbuf.rd8(off)
-    }
-
+impl<'a, T, Q, const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize>
+ByteBuf for IPCBufRef<'a, T, Q, IPCBUFNR, DEVMEMNR, CHL, CHNR> {
     fn wr8(&mut self, off: usize, value: u8) {
-        let Some(mut inner) = self.rt.ipcbufbuf.take() else {
-            return;
-        };
-        let Some(ipcbuf) = inner.iter_mut().nth(self.ipcbuf) else {
-            return;
-        };
-        ipcbuf.wr8(off, value);
-    }
-}
-
-/*
- * runtime
- */
-pub struct RuntimeMain<'a, T, Q, const BUFNR: usize, const CHL: usize, const CHNR: usize> {
-    timer: Cell<Option<T>>,
-    queue: Cell<Q>,
-    logbufbuf: RefCell<LogBufBuf<CHL, CHNR>>,
-    ipcbufbuf: Cell<Option<Deque<IPCByteBuf<'a>, BUFNR>>>,
-}
-
-impl<'a, T, Q, const BUFNR: usize, const CHL: usize, const CHNR: usize>
-RuntimeMain<'a, T, Q, BUFNR, CHL, CHNR> {
-    pub fn new<B: FnMut(usize) -> IPCByteBuf<'a>>(timer: T, queue: Q, mut bufctr: B) -> Self {
-        Self {
-            timer: Cell::new(Some(timer)), queue: Cell::new(queue),
-            ipcbufbuf: Cell::new(Some(Deque::new(|idx| bufctr(idx)))),
-            logbufbuf: RefCell::new(LogBufBuf::default()),
+        if let Some(buf) = self.rt.ipcbufbuf.borrow_mut().iter_mut().nth(self.idx) {
+            buf.wr8(off, value);
         }
     }
-}
 
-impl<'a, T, Q, const BUFNR: usize, const CHL: usize, const CHNR: usize>
-RuntimeMain<'a, T, Q, BUFNR, CHL, CHNR>
-where T: Time {
-    pub fn as_ref(&'a self) -> impl Runtime {
-        RuntimeRef {
-            rt: self, logbuf: 0, ipcbuf: 0,
+    fn rd8(&mut self, off: usize) -> u8 {
+        if let Some(buf) = self.rt.ipcbufbuf.borrow_mut().iter_mut().nth(self.idx) {
+            buf.rd8(off)
+        }
+        0
+    }
+
+    fn wr16(&mut self, off: usize, value: u16) {
+        if let Some(buf) = self.rt.ipcbufbuf.borrow_mut().iter_mut().nth(self.idx) {
+            buf.wr16(off, value);
         }
     }
+
+    fn rd16(&mut self, off: usize) -> u16 {
+        let mut buf = self.rt.ipcbufbuf.borrow_mut();
+        let Some(bytebuf) = buf.iter_mut().nth(self.idx) else {
+            return 0;
+        };
+        bytebuf.rd16(off)
+    }
 }
+
+#[derive(Clone, Copy)]
+struct DevBufRef<'a, T, Q,
+const IPCBUFNR: usize, const DEVMEMNR: usize, const CHL: usize, const CHNR: usize> {
+    devbuf: usize,
+    rt: &'a RuntimeMain<'a, T, Q, IPCBUFNR, DEVMEMNR, CHL, CHNR>,
+}
+
 
 /*
  * log buffer buffer
